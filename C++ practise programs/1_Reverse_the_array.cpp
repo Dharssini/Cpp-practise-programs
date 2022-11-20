f188f76f5bb03a9c065e91f1df19deb8490aefcd
Dharssini <dharssinikarthikeyan@gmail.com>
2022-11-20 14:30:00 +0530

    #question 4

--- C++ practise programs/1_Reverse_the_array.cpp
+++ C++ practise programs/1_Reverse_the_array.cpp
@@ -1,12 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-     int a[5]={5,2,6,9,7};
-     int n=sizeof(a);
-     for(int i=n;i<0;i--)
-     {
-          cout<<a[i];
-     }
 
+// initialize array
+int arr[] = {20, 34, 5, 8, 1, 78};
+
+// size of the array
+int size_array = sizeof(arr) / sizeof(arr[0]);
+
+void reverseArr(int arr[], int num) {
+  // check the size of array
+  if (num == size_array)
+    return;
+  // extract array elements
+  int elem = arr[num];
+  // recursively calls the next element of the array
+  reverseArr(arr, num + 1);
+  // assigning elements
+  arr[size_array - num - 1] = elem;
+}
+
+int main() {
+  int i;
+  // call recursive function (start from first elements
+  cout << " Original elements of the arrays " << endl;
+  for (int i = 0; i < size_array; i++) {
+    cout << arr[i] << " ";
+  }
+
+  reverseArr(arr, 0);
+  cout << " \n Reverse elements of the array are: " << endl;
+  // display the array elements
+  for (int i = 0; i < size_array; i++) {
+    cout << arr[i] << " ";
+  }
+  return 0;
 }
---
+++ C++ practise programs/4_sort_without_algo_[0,1,2].md
@@ -0,0 +1,11 @@
+# Sort an array of 0s, 1s and 2s without Algorithm 
+
+## What the problem is?
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+## Basic understanding:
+
+
+### Brute Force Approach:
+
+
